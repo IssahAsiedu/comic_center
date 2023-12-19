65cc2ffe8f1d42e355e4b2238a4bda1773b5dec0
Issah Asiedu <issah.asiedu@amalitech.com>
2023-12-19 11:25:25 +0000

    show bookmarked items during fetch

--- lib/presentation/screen_sections/home_all_comics.dart
+++ lib/presentation/screen_sections/home_all_comics.dart
@@ -2,24 +2,29 @@ import 'package:comics_center/domain/comic/comic.dart';
 import 'package:comics_center/infrastructure/network/response.dart';
 import 'package:comics_center/infrastructure/network/rest_client.dart';
 import 'package:comics_center/presentation/comic/widgets/home_comic_card.dart';
+import 'package:comics_center/providers/app_providers.dart';
+import 'package:comics_center/providers/auth/auth.dart';
+import 'package:comics_center/providers/auth/auth_state.dart';
 import 'package:comics_center/routing/route_config.dart';
 import 'package:comics_center/shared/app_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
-class HomeAllComicsSection extends StatefulWidget {
+class HomeAllComicsSection extends ConsumerStatefulWidget {
   const HomeAllComicsSection({super.key});
 
   @override
-  State<HomeAllComicsSection> createState() => _HomeAllComicsSectionState();
+  ConsumerState<HomeAllComicsSection> createState() =>
+      _HomeAllComicsSectionState();
 }
 
-class _HomeAllComicsSectionState extends State<HomeAllComicsSection> {
+class _HomeAllComicsSectionState extends ConsumerState<HomeAllComicsSection> {
   final PagingController<int, Comic> _comicsPagingController =
       PagingController(firstPageKey: 0);
 
-  int limit = 10;
+  int limit = 7;
 
   @override
   void initState() {
@@ -100,14 +105,29 @@ class _HomeAllComicsSectionState extends State<HomeAllComicsSection> {
       return;
     }
 
+    final comics = response.data!.data;
+    final table =
+        ref.read(supabaseClientProvider).from(AppStrings.bookmarksTable);
+    final authState = ref.read(authProvider);
+
+    if (authState is AuthSuccess) {
+      for (var comic in comics) {
+        List<dynamic>? result = await table
+            .select()
+            .eq("userid", authState.user.id)
+            .eq('id', comic.id);
+        if (result != null && result.isNotEmpty) comic.bookMarked = true;
+      }
+    }
+
     var pages = (response.data!.total / limit).ceil();
     if (pageKey == pages && mounted) {
-      _comicsPagingController.appendLastPage(response.data!.data);
+      _comicsPagingController.appendLastPage(comics);
       return;
     }
 
     if (!mounted) return;
-    _comicsPagingController.appendPage(response.data!.data, ++pageKey);
+    _comicsPagingController.appendPage(comics, ++pageKey);
   }
 
   bool _isLastItem(int index) {
