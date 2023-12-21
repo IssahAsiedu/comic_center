cb19dc364fdc11a66a839979114faa1867d06db2
Issah Asiedu <issah.asiedu@amalitech.com>
2023-12-21 20:21:59 +0000

    remove item from bookmarks when state is changed on the details screen

--- lib/presentation/bookmark/bookmark_card.dart
+++ lib/presentation/bookmark/bookmark_card.dart
@@ -1,31 +1,17 @@
 import 'package:comics_center/domain/bookmark.dart';
 import 'package:comics_center/presentation/widgets/app_bar/home_app_bar.dart';
-import 'package:comics_center/routing/route_config.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class BookmarkCard extends StatelessWidget {
-  const BookmarkCard({
-    super.key,
-    required this.bookmark,
-  });
+  const BookmarkCard({super.key, required this.bookmark, this.onTap});
 
   final Bookmark bookmark;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        if (bookmark.type.toLowerCase() == "series") {
-          var route = AppRouteNotifier.generateSeriesRoute(bookmark.id);
-          context.push(route);
-        }
-
-        if (bookmark.type.toLowerCase() == "comic") {
-          var route = AppRouteNotifier.generateComicRoute(bookmark.id);
-          context.push(route);
-        }
-      },
+      onTap: onTap,
       child: Row(
         children: [
           // image
--- lib/presentation/screens/home/home_bookmarks.dart
+++ lib/presentation/screens/home/home_bookmarks.dart
@@ -1,3 +1,4 @@
+import 'package:comics_center/domain/book_markable.dart';
 import 'package:comics_center/domain/bookmark.dart';
 import 'package:comics_center/presentation/bookmark/bookmark_card.dart';
 import 'package:comics_center/presentation/widgets/back_button.dart';
@@ -5,9 +6,11 @@ import 'package:comics_center/providers/app_providers.dart';
 import 'package:comics_center/providers/auth/auth.dart';
 import 'package:comics_center/providers/auth/auth_state.dart';
 import 'package:comics_center/providers/home/home_providers.dart';
+import 'package:comics_center/routing/route_config.dart';
 import 'package:comics_center/shared/app_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class HomeBookmarksScreen extends ConsumerStatefulWidget {
@@ -73,7 +76,35 @@ class _HomeBookmarksScreenState extends ConsumerState<HomeBookmarksScreen> {
                       margin: EdgeInsets.only(
                           top: index == 0 ? 20 : 0,
                           bottom: _isLastItem(index) ? 50 : 0),
-                      child: BookmarkCard(bookmark: bookmark),
+                      child: BookmarkCard(
+                        bookmark: bookmark,
+                        onTap: () async {
+                          Bookmarkable? value;
+                          if (bookmark.type.toLowerCase() == "series") {
+                            var route = AppRouteNotifier.generateSeriesRoute(
+                                bookmark.id);
+                            value = await context.push(route);
+                          }
+
+                          if (bookmark.type.toLowerCase() == "comic" &&
+                              mounted) {
+                            var route = AppRouteNotifier.generateComicRoute(
+                                bookmark.id);
+                            value = await context.push(route);
+                          }
+
+                          if (value == null) return;
+                          if (value.bookMarked) return;
+
+                          var items = _bookmarksPagingController.itemList
+                              ?.where((element) {
+                            return element.pk != bookmark.pk;
+                          }).toList();
+
+                          if (items == null) return;
+                          _bookmarksPagingController.itemList = items;
+                        },
+                      ),
                     );
                   },
                 ),
